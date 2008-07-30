330e9cb05ab612fbe981748975a935992622e86e
hugang <hugang@hugang-macbook.(none)>
2008-07-30 21:27:54 +0800

    modified:   decode_in.v
            update.

--- decode/rtl/verilog/decode_in.v
+++ decode/rtl/verilog/decode_in.v
@@ -33,7 +33,7 @@ module decode_in (/*AUTOARG*/
    output 	 stream_valid;
 
    /* first we split 64 bit to 16 bit */
-   reg 		 pull, pull_n;
+   reg 		 pull_n;
    reg [1:0] 	 cnt;
    always @(posedge clk or posedge rst)
      begin
@@ -66,12 +66,10 @@ module decode_in (/*AUTOARG*/
 	   sreg <= #1 0;
 	   left <= #1 0;
 	   state<= #1 0;
-	   pull <= #1 0;
 	end else begin
 	   sreg <= #1 sreg_n;
 	   left <= #1 left_n;
 	   state<= #1 state_n;
-	   pull <= #1 pull_n;
 	end
      end
    
@@ -84,20 +82,17 @@ module decode_in (/*AUTOARG*/
 	left_n  = left;
 	
 	case (state)
-	  2'b00: if (~src_empty && ce)
-	    state_n = 2'b01;
-	  
-	  2'b01: begin
-	     sreg_n = {fi[07:00], fi[15:08], fi[23:16], fi[31:24]};
-	     left_n = 32;
-	     state_n = 2'b11;
+	  2'b00: if (~src_empty && ce) begin
+	     sreg_n  = {fi[07:00], fi[15:08], fi[23:16], fi[31:24]};
+	     left_n  = 32;
+	     state_n = 2'b01;
 	  end
 	  
-	  2'b11: begin
+	  2'b01: begin
 	     if (stream_ack) begin
 		if ((left - stream_width) < 5'h10) begin
 		   sreg_n = (sreg << stream_width) |
-			     (data << (5'h10 - left + stream_width));
+			    (data << (5'h10 - left + stream_width));
 		   left_n = left - stream_width + 5'h10;
 		   pull_n = 1'b1;
 		end else begin
@@ -105,11 +100,11 @@ module decode_in (/*AUTOARG*/
 		   left_n = left - stream_width;
 		end
 	     end // if (stream_ack)
-	  end // case: 2'b11
+	  end // case: 2'b01
 	  
 	endcase // case(state)
      end // always @ (...
-
+   
    assign stream_data = sreg[31:19];
    assign stream_valid= |{left[5:4]};
    
