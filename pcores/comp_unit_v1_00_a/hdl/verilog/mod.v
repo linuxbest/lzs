44cb594d3e0eabfa7d89a4632a509c0c3426baee
songjun <songjun@songjun-laptop.(none)>
2010-06-06 10:18:29 +0800

    add debug

--- pcores/comp_unit_v1_00_a/hdl/verilog/mod.v
+++ pcores/comp_unit_v1_00_a/hdl/verilog/mod.v
@@ -14,10 +14,13 @@
 module mod(/*AUTOARG*/
    // Outputs
    m_src_getn, m_dst_putn, m_dst, m_dst_last, m_endn, m_cap,
+   en_out_data, en_out_done, en_out_valid, cnt_finish, data_empty,
+   data_valid, encode_ctl_state, encode_dp_state, encode_out_state,
+   de_out_data, de_out_done, de_out_valid, decode_ctl_state,
+   decode_stream_done, decode_out_done,
    // Inputs
    wb_clk_i, m_reset, m_enable, dc, m_src, m_src_last,
-   m_src_almost_empty, m_src_empty, m_dst_almost_full,
-   m_dst_full
+   m_src_almost_empty, m_src_empty, m_dst_almost_full, m_dst_full
    );
    input wb_clk_i;
    input m_reset;
@@ -42,18 +45,36 @@ module mod(/*AUTOARG*/
 
    output [7:0]  m_cap;
    
+   //debug signal
+   output 	en_out_data;
+   output 	en_out_done;
+   output 	en_out_valid;
+   output 	cnt_finish;
+   output 	data_empty;
+   output 	data_valid;
+   output [2:0]	encode_ctl_state;
+   output [2:0]	encode_dp_state;
+   output [2:0]	encode_out_state;
+
+   output	de_out_data; 
+   output 	de_out_done;
+   output 	de_out_valid;
+   output [2:0]	decode_ctl_state;
+   output 	decode_stream_done;
+   output 	decode_out_done;
+
    // synopsys translate_off
    pullup(m_dst_putn);
    pullup(m_src_getn);
- //  pullup(m_endn);
-   // synopsys translate_on
-
+   pullup(m_endn);
+  
    wire 	 fo_full   = m_dst_full  || m_dst_almost_full;
    wire 	 src_empty = m_src_empty || m_src_almost_empty;
 
    wire [15:0] 	 en_out_data,  de_out_data;
    wire 	 en_out_valid, de_out_valid;
    wire 	 en_out_done,  de_out_done;
+   
    encode encode(.ce(dc[5] && m_enable),
 		 .fi(m_src),
 		 .clk(wb_clk_i),
@@ -64,7 +85,13 @@ module mod(/*AUTOARG*/
 		 .m_last(m_src_last),
 		 /*AUTOINST*/
 		 // Outputs
+		 .encode_ctl_state	(encode_ctl_state[2:0]),
+		 .encode_dp_state	(encode_dp_state[2:0]),
+		 .encode_out_state	(encode_out_state[2:0]),
 		 .m_src_getn		(m_src_getn),
+		 .cnt_finish		(cnt_finish),
+		 .data_empty		(data_empty),
+		 .data_valid		(data_valid),
 		 // Inputs
 		 .fo_full		(fo_full),
 		 .src_empty		(src_empty));
@@ -79,7 +106,10 @@ module mod(/*AUTOARG*/
 		 .m_last(m_src_last),
 		 /*AUTOINST*/
 		 // Outputs
+		 .decode_ctl_state	(decode_ctl_state),
 		 .m_src_getn		(m_src_getn),
+		 .decode_stream_done	(decode_stream_done),
+		 .decode_out_done	(decode_out_done),
 		 // Inputs
 		 .fo_full		(fo_full),
 		 .src_empty		(src_empty));
